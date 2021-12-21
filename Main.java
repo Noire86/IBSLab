052b034ac36853eeb71433b2a82c53c56feb349e
Noire86 <gleb.iwanow2010@yandex.ru>
2021-12-21 22:26:12 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello! This is a git test task!");
+        System.out.println("Lets add something");
     }
+
 }
